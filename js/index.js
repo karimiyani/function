cb87433e3e7457b40b1745d7827e333d0d23943f
karimiyani <front_end2020@yahoo.com>
2021-10-11 23:21:18 +0330

    first commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// function declration
+speak();
+function speak (){
+    console.log("speak !")
+}
+speak();
+
+
+// *********************
+
+// function expression
+// run();     //error we can not call before 
+const run =function run (){
+    console.log("run !")
+}
+run();
